5ebdbc3cee232b86e27d36b1fa9bcf305742adf6
hking2 <hking6776@gmail.com>
2020-11-09 13:58:19 -0800

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react'
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Welcome to Cindy's Tienda</h1>
+    </Fragment>
+  );
+}
+
+export default App;
